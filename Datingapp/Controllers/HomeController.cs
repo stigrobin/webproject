e2d5f6b21ab7a7b7a3557729ed25804d7ca0909c
Erik <Erik@LAPTOP-M0GJMAFE>
2017-12-29 12:46:16 +0100

    EN jävla massa

--- Datingapp/Controllers/HomeController.cs
+++ Datingapp/Controllers/HomeController.cs
@@ -12,19 +12,12 @@ namespace DatingApp.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        
+        [Authorize]
+        public ActionResult EditProfile()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
