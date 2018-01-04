172d10a8743556b9669257eaac98a17502ac32d1
Robin <stigrobin@hotmail.se>
2018-01-04 12:41:54 +0100

    ingaerrors

---
+++ DatingApp/Controllers/Api/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using DatingApp.Models;
+using DomainLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DatingApp.Controllers.Api
+{
+    public class MessageController : ApiController
+    {
+        DataContext dataContext = new DataContext();
+
+        [HttpPost]
+        public Message PostMessage(string msgContent)
+        {
+            Message message = new Message { Content = msgContent };
+            dataContext.Messages.Add(message);
+            return message;
+        }
+    }
+}
