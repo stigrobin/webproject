de6f31e95a82609bc9bf4bf0420131ed430aa1b1
Robin <stigrobin@hotmail.se>
2018-01-05 11:29:54 +0100

    profilerespository

--- Datingapp/Controllers/HomeController.cs
+++ Datingapp/Controllers/HomeController.cs
@@ -17,28 +17,18 @@ namespace DatingApp.Controllers
         public ActionResult Index()
         {
             int count = 0;
-            var profiles = dataContext.Profiles.Where(x => x.FileName != null);
+            var profiles = dataContext.Profiles.Where(x => x.FileName != null).ToList();
             Random random = new Random();
             //int Profilecount = profiles.Count();
-            List<string> sources = new List<string>();
-            List<Profile> profilesWithPic = new List<Profile>();
+            List<Profile> sources = new List<Profile>();
 
             //if inga bilder finns
-            if (profilesWithPic.Count() > 0)
+            if (profiles.Count() > 0)
             {
-                foreach (var item in profiles)
-                {
-                    profilesWithPic.Add(item);
-
-                }
                 for (int i = 0; i < 3; i++)
                 {
-                    int randomNumber = random.Next(0, profilesWithPic.Count());
-                    //var id = profilesWithPic.Where(x => x.ProfileId == randomNumber);
-                    var pic = (from Profile in profiles where Profile.ProfileId == randomNumber select Profile.Content).First();
-                    var base64 = Convert.ToBase64String(pic);
-                    var imgSrc = String.Format("data:image/gif;base64,{0}", base64);
-                    sources.Add(imgSrc);
+                    int randomNumber = random.Next(0, profiles.Count());
+                    sources.Add(profiles[randomNumber]);
                     count++;
                 }
             }
---
+++ DomainLibrary/Repositories/ProfileRepository.cs
@@ -0,0 +1,20 @@
+﻿using DomainLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLibrary.Repositories
+{
+    public class ProfileRepository
+    {
+        public string GetImageSrc(Profile profile)
+        {
+            var pic = profile.Content;
+            var base64 = Convert.ToBase64String(pic);
+            var imgSrc = String.Format("data:image/gif;base64,{0}", base64);
+            return imgSrc;
+        }
+    }
+}
